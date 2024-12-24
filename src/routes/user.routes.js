3388db5894283f894e8428007faf38006c247400
Aksh10x <akshathsurwase@gmail.com>
2024-12-24 20:11:15 +0530

    endpoints created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import {Router} from "express";
-import { registerUser , loginUser , logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser , 
+    logoutUser,
+    refreshAccessToken,
+    getCurrentUser, 
+    changeCurrentPassword,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -24,5 +34,21 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refreshToken").post(refreshAccessToken)
+
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/updateAvatar").patch(verifyJWT, 
+    upload.single("avatar"),
+    updateUserAvatar)
+
+router.route("/updateCoverImage").patch(verifyJWT, 
+    upload.single("coverImage"),
+    updateUserCoverImage)
+
+router.route("/getChannel/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;   // can import like: import router from (no {})
